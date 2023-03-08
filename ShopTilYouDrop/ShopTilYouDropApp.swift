00dac8aa38807b227170d77e39254d24d92a3fcf
Nicholas Cammisuli <cammisun@sheridancollege.ca>
2023-03-08 10:55:18 -0500

    Initial Commit

---
+++ ShopTilYouDrop/ShopTilYouDropApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopTilYouDropApp.swift
+//  ShopTilYouDrop
+//
+//  Created by Nichoalas Cammisuli on 2023-03-08.
+//
+
+import SwiftUI
+
+@main
+struct ShopTilYouDropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
