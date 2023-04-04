3c217972fe4b09f241282267c506f3ce90210647
Nicholas Cammisuli <cammisun@sheridancollege.ca>
2023-04-03 23:25:53 -0400

    Created the possibility to add photo from library - NOT COMPLETED

--- ShopTilYouDrop/Views/Scan.swift
+++ ShopTilYouDrop/Views/Scan.swift
@@ -6,11 +6,16 @@
 //
 
 import SwiftUI
+import Photos
+import PhotosUI
 
 
 struct Scan: View {
     
     @State private var selection: Int? = nil
+    @State private var selectedImage: UIImage? = nil
+    
+//    private var coordinator: Coordinator? = nil
     
     var body: some View {
         
@@ -26,14 +31,25 @@ struct Scan: View {
                 .font(.largeTitle)
                 .underline()
             
-            Image("photo")
-                .resizable()
-                .frame(width: 175, height: 175)
-                .overlay(
-                    Rectangle()
-                        .stroke(Color.black, lineWidth: 2)
-                        .padding(-50)
-                )
+            if let image = selectedImage {
+                Image(uiImage: image)
+                    .resizable()
+                    .frame(width: 175, height: 175)
+                    .overlay(
+                        Rectangle()
+                            .stroke(Color.black, lineWidth: 2)
+                            .padding(-50)
+                    )
+            } else {
+                Image("photo")
+                    .resizable()
+                    .frame(width: 175, height: 175)
+                    .overlay(
+                        Rectangle()
+                            .stroke(Color.black, lineWidth: 2)
+                            .padding(-50)
+                    )
+            }
             
             
             NavigationLink(destination: CameraView(), tag: 1, selection: self.$selection){}
@@ -61,10 +77,62 @@ struct Scan: View {
                 
             }.onTapGesture {
                 
+               didTapAdd()
+                
+                
             }.padding(-50)
             
         }.offset(y: 0)
     }
+    
+    func didTapAdd(){
+        var config = PHPickerConfiguration()
+        config.filter = .images
+        config.selectionLimit = 1
+        
+        let picker = PHPickerViewController(configuration: config)
+//        coordinator = Coordinator(self)
+        picker.delegate = Coordinator(self)
+        //        UIApplication.shared.windows.first?.rootViewController?.present(vc, animated: true)
+        if let topWindow = UIApplication.shared.connectedScenes
+            .compactMap({ $0 as? UIWindowScene })
+            .flatMap({ $0.windows })
+            .first(where: { $0.isKeyWindow }) {
+            topWindow.rootViewController?.present(picker, animated: true)
+        }
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(self)
+    }
+
+    class Coordinator: NSObject, PHPickerViewControllerDelegate {
+        var parent: Scan
+        
+        init(_ parent: Scan) {
+            self.parent = parent
+        }
+        
+        func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+            picker.dismiss(animated: true, completion: nil)
+            guard let itemProvider = results.first?.itemProvider else {
+                return
+            }
+            if itemProvider.canLoadObject(ofClass: UIImage.self) {
+                itemProvider.loadObject(ofClass: UIImage.self) { [weak self] image, error in
+                    if let image = image as? UIImage {
+                        DispatchQueue.main.async {
+                            self?.parent.selectedImage = image
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+        
+        
+    
 }
 
 struct Scan_Previews: PreviewProvider {
