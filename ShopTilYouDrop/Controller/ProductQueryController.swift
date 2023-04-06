1fdc85d6e77974cc178b9f2155f07e89436ff698
Jordan Bhar <48815489+JordanBhar@users.noreply.github.com>
2023-04-06 10:50:46 -0400

    Added Wishlist Button and Loading animations

--- ShopTilYouDrop/Controller/ProductQueryController.swift
+++ ShopTilYouDrop/Controller/ProductQueryController.swift
@@ -8,6 +8,15 @@
 import Foundation
 import UIKit
 import SwiftUI
+import Network
+
+enum NetworkError: Error {
+    case invalidURL
+    case invalidResponse
+    case statusCode(Int)
+    case emptyData
+}
+
 
 class ProductQueryController : ObservableObject{
     
@@ -15,67 +24,106 @@ class ProductQueryController : ObservableObject{
     
     let group = DispatchGroup()
     //@Published var productDataQuery = Product()
-
-    func fetchDataFromAPI(product : String){
-        print("Fetching data from API called")
-        guard let api = URL(string: "https://api.amzapi.com/v1/search?apikey=40ad5530-d428-11ed-b110-05c5eb371804&query=\(product)") else {
-            print(#function, "Unable to obtain from string")
-            return
+    
+    func fetchProductData(for product: String, completion: @escaping (Result<Product, Error>) -> Void) {
+            guard let url = URL(string: "https://api.amzapi.com/v1/search?apikey=40ad5530-d428-11ed-b110-05c5eb371804&query=\(product)") else {
+                completion(.failure(NetworkError.invalidURL))
+                return
+            }
+            
+            URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
+                if let error = error {
+                    completion(.failure(error))
+                    return
+                }
+                
+                guard let httpResponse = response as? HTTPURLResponse else {
+                    completion(.failure(NetworkError.invalidResponse))
+                    return
+                }
+                
+                guard (200..<300).contains(httpResponse.statusCode) else {
+                    completion(.failure(NetworkError.statusCode(httpResponse.statusCode)))
+                    return
+                }
+                
+                guard let data = data else {
+                    completion(.failure(NetworkError.emptyData))
+                    return
+                }
+                
+                do {
+                    let decoder = JSONDecoder()
+                    let product = try decoder.decode(Product.self, from: data)
+                    self?.productData.append(product)
+                    completion(.success(product))
+                } catch {
+                    completion(.failure(error))
+                }
+            }.resume()
         }
 
 
-        let task = URLSession.shared.dataTask(with: api) { (data: Data?, response: URLResponse?, error) in
-
-
-            if let error = error {
-                print(#function, "Unable to establish network connection \(error)")
-            } else {
-
-                if let httpResponse = response as? HTTPURLResponse {
-
-                    if httpResponse.statusCode == 200 {
-                        //200 response OK
-                        DispatchQueue.global().async {
-                            do{
-
-                                if(data != nil){
-
-                                    if let jsonData = data {
-
-
-                                        let decodedData = try JSONDecoder().decode(Product.self, from: jsonData)
-
-
-                                        DispatchQueue.main.async{
-                                            self.productData.append(decodedData)
-
-                                        }
-
-                                    } else {
-                                        print(#function, "Unable to obtain jsonData")
-                                    }
-
-                                } else {
-                                    print(#function, "No data with response code 200")
-                                }
-
-                            }catch let error {
-                                print(error)
-
-                            }
-                        }
-                    }
-
-                } else {
-                    print(#function, "Unable to recieve any response")
-                }// else
-
-            }//else
-
-        }
-
-        task.resume()
-    }
+//    func fetchDataFromAPI(product : String){
+//        print("Fetching data from API called")
+//        guard let api = URL(string: "https://api.amzapi.com/v1/search?apikey=40ad5530-d428-11ed-b110-05c5eb371804&query=\(product)") else {
+//            print(#function, "Unable to obtain from string")
+//            return
+//        }
+//
+//
+//        let task = URLSession.shared.dataTask(with: api) { (data: Data?, response: URLResponse?, error) in
+//
+//
+//            if let error = error {
+//                print(#function, "Unable to establish network connection \(error)")
+//            } else {
+//
+//                if let httpResponse = response as? HTTPURLResponse {
+//
+//                    if httpResponse.statusCode == 200 {
+//                        //200 response OK
+//                        DispatchQueue.global().async {
+//                            do{
+//
+//                                if(data != nil){
+//
+//                                    if let jsonData = data {
+//
+//
+//                                        let decodedData = try JSONDecoder().decode(Product.self, from: jsonData)
+//
+//
+//                                        DispatchQueue.main.async{
+//                                            self.productData.append(decodedData)
+//
+//                                        }
+//
+//                                    } else {
+//                                        print(#function, "Unable to obtain jsonData")
+//                                    }
+//
+//                                } else {
+//                                    print(#function, "No data with response code 200")
+//                                }
+//
+//                            }catch let error {
+//                                print(error)
+//
+//                            }
+//                        }
+//                    }
+//
+//                } else {
+//                    print(#function, "Unable to recieve any response")
+//                }// else
+//
+//            }//else
+//
+//        }
+//
+//        task.resume()
+//    }
     
 //    func fetchDataFromAPII(product : String, withCompletion completion: @escaping (Product?) -> Void){
 //        print("Fetching data from API called")
@@ -138,6 +186,11 @@ class ProductQueryController : ObservableObject{
 //    }
     
     func getSpecificProductData(index : Int) -> Product{
+        
+        
+        
+        
+        
         return productData[index]
     }
     
--- ShopTilYouDrop/Views/Search.swift
+++ ShopTilYouDrop/Views/Search.swift
@@ -7,6 +7,9 @@
 
 import SwiftUI
 
+
+import SwiftUI
+
 struct Search: View {
     
     @State var productList : [String]
@@ -14,6 +17,7 @@ struct Search: View {
     @State private var selection : Int? = nil
     @State private var selectedProduct : String = ""
     @State private var product : Product = Product()
+    @State private var isLoading = false
     
     @EnvironmentObject var productHelper : ProductQueryController
 
@@ -57,9 +61,20 @@ struct Search: View {
                             .textInputAutocapitalization(.words)
                         
                         Button(action:{
+                            isLoading = true
                             productList.append(searchProduct)
                             //maybe create a external function that will wait for the data to be gathered
-                            self.productHelper.fetchDataFromAPI(product: searchProduct)
+                            self.productHelper.fetchProductData(for: searchProduct, completion: { result in
+                                switch result {
+                                case .success(let product):
+                                    print("Fetched product: \(product)")
+                                    self.isLoading = false
+                                    
+                                case .failure(let error):
+                                    print("Error fetching product: \(error)")
+                                    self.isLoading = false
+                                }
+                            })
                         }){
                             Text("Search")
                                 .foregroundColor(.white)
@@ -85,7 +100,14 @@ struct Search: View {
                                     
                                     Spacer().lineLimit(5)
                                     
-                                    Button(action : {}){ Image(systemName: "trash").font(.title).foregroundColor(Color.white)}
+                                    Button(action : {}){ Image(systemName: "heart").font(.largeTitle).foregroundColor(Color.white)}
+                                    .background(Color.blue)
+                                    .cornerRadius(10)
+                                    .onTapGesture {
+                                        
+                                    }
+                                    
+                                    Button(action : {}){ Image(systemName: "trash").font(.largeTitle).foregroundColor(Color.white)}
                                     .background(Color.red)
                                     .cornerRadius(10)
                                     .onTapGesture {
@@ -94,15 +116,26 @@ struct Search: View {
                                     }
                                     
                                     
-                                    Button(action: {}, label: {Text("Compare")
-                                            .foregroundColor(.white)
-                                            .padding(10)})
-                                        .background(Color.blue)
-                                        .cornerRadius(10)
-                                        .onTapGesture {
-                                        self.selectedProduct = currentProduct
-                                        self.product = self.productHelper.getSpecificProductData(index: index)
-                                        self.selection = 1
+                                    Button(action: {}, label: {
+                                        if isLoading {
+                                            ProgressView()
+                                                .padding(10)
+                                                .foregroundColor(.white)
+                                        } else {
+                                            Text("Compare")
+                                                .padding(10)
+                                                .foregroundColor(.white)
+                                        }
+                                    })
+                                    .background(Color.green)
+                                    .cornerRadius(10)
+                                    .disabled(isLoading)
+                                    .onTapGesture {
+                                        if !isLoading {
+                                            self.selectedProduct = currentProduct
+                                            self.product = self.productHelper.getSpecificProductData(index: index)
+                                            self.selection = 1
+                                        }
                                     }
                                 }
                             }
@@ -113,6 +146,120 @@ struct Search: View {
         }
     }
 }
+
+//struct Search: View {
+//
+//    @State var productList : [String]
+//    @State var searchProduct : String = ""
+//    @State private var selection : Int? = nil
+//    @State private var selectedProduct : String = ""
+//    @State private var product : Product = Product()
+//
+//    @EnvironmentObject var productHelper : ProductQueryController
+//
+//
+//    var body: some View {
+//        NavigationView{
+//            VStack{
+//
+//                NavigationLink(destination: ProductCompair(_productName: selectedProduct , productObject : product ), tag : 1, selection: self.$selection ){}
+//
+//                VStack{
+//                    HStack{
+//                        Text("Product Search")
+//                            .font(.largeTitle)
+//                            .bold()
+//                        Image(systemName: "magnifyingglass")
+//                            .font(.title2)
+//                    }
+//                }
+//
+//                Group{
+//
+//                    HStack{
+//
+//                        Button(action:{
+//
+//                        }){
+//                            Image(systemName: "line.3.horizontal.decrease.circle.fill")
+//                                .font(.title)
+//                        }
+//
+//                        TextField("Enter Product", text: $searchProduct)
+//                            .padding(EdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10))
+//                            .overlay(
+//                                RoundedRectangle(cornerRadius: 16)
+//                                        .stroke(.blue, lineWidth: 3)
+//
+//                                )
+//                            .frame(width:  UIScreen.main.bounds.width - 150)
+//                            .disableAutocorrection(true)
+//                            .textInputAutocapitalization(.words)
+//
+//                        Button(action:{
+//                            productList.append(searchProduct)
+//                            //maybe create a external function that will wait for the data to be gathered
+//                            self.productHelper.fetchProductData(for: searchProduct, completion: { result in
+//                                switch result {
+//                                                case .success(let product):
+//                                                    print("Fetched product: \(product)")
+//                                                case .failure(let error):
+//                                                    print("Error fetching product: \(error)")
+//                                                }
+//                            })
+//                        }){
+//                            Text("Search")
+//                                .foregroundColor(.white)
+//                                .padding(10)
+//                        }
+//                        .background(Color.blue)
+//                        .cornerRadius(10)
+//
+//                    }
+//
+//                    Divider()
+//                }
+//
+//                List{
+//                    if(!productList.isEmpty){
+//                        ForEach (productList.enumerated().map({$0}), id: \.element.self){index , currentProduct in
+//                            VStack{
+//
+//
+//                                HStack{
+//                                    Text(currentProduct)
+//                                        .bold()
+//
+//                                    Spacer().lineLimit(5)
+//
+//                                    Button(action : {}){ Image(systemName: "trash").font(.largeTitle).foregroundColor(Color.white)}
+//                                    .background(Color.red)
+//                                    .cornerRadius(10)
+//                                    .onTapGesture {
+//                                        self.productList.remove(at: index)
+//                                        self.productHelper.productData.remove(at: index)
+//                                    }
+//
+//
+//                                    Button(action: {}, label: {Text("Compare")
+//                                            .foregroundColor(.white)
+//                                            .padding(10)})
+//                                        .background(Color.blue)
+//                                        .cornerRadius(10)
+//                                        .onTapGesture {
+//                                        self.selectedProduct = currentProduct
+//                                        self.product = self.productHelper.getSpecificProductData(index: index)
+//                                        self.selection = 1
+//                                    }
+//                                }
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//    }
+//}
 
 struct Search_Previews: PreviewProvider {
     static var previews: some View {
